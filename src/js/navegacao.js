757a73407d092e42d3ae92a24124997d8068f801
bruno-b-neves <brunobningles@gmail.com>
2023-02-17 17:07:43 -0300

    VERSION 1.0

---
+++ src/js/navegacao.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('[spa-nav]').forEach(link => {
+    const conteudo = document.getElementById('conteudo')
+    link.onclick = function (e) {
+        e.preventDefault()
+        fetch(link.getAttribute('spa-nav'))
+            .then(resp => resp.text())
+            .then(html => conteudo.innerHTML = html)
+    }
+})
